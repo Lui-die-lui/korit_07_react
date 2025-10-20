475f7e5966fc5ba407994b514bc08286aec1af77
Lui-die-lui <lsg05h@naver.com>
2025-10-20 17:47:17 +0900

    feat: 20251020 frontend RESTful API / AG Grid / MUI

--- carfront/src/App.tsx
+++ carfront/src/App.tsx
@@ -1,13 +1,29 @@
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Carlist from "./components/Carlist";
 
-import './App.css'
+const queryClient = new QueryClient();
 
 function App() {
-
   return (
-    <>
-  
-    </>
-  )
+    <Container maxWidth="xl">
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Car Shop</Typography>
+        </Toolbar>
+      </AppBar>
+      <QueryClientProvider client={queryClient}>
+        <Carlist />
+      </QueryClientProvider>
+    </Container>
+  );
 }
 
-export default App
+export default App;
